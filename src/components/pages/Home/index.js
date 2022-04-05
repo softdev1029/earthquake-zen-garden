36c12cdefff0aaa611539cdf7c135b9cd7fa58b9
softdev1029 <softdev1029@outlook.com>
2022-04-05 21:50:07 +0300

    Complete Home list page

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,7 +1,34 @@
 import React from "react";
 
+import data from "mock/data.json";
+
+import * as S from "./style";
+
 function Home() {
-  return <div>Home</div>;
+  const list = data.data.features.map((e) => {
+    return (
+      <tr key={e.id}>
+        <td>{e.properties.place}</td>
+        <td>{e.properties.mag}</td>
+        <td>{e.properties.time}</td>
+      </tr>
+    );
+  });
+  return (
+    <S.Wrapper>
+      <h2>USGS All Earthquakes, Past Hour</h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Magnitude</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    </S.Wrapper>
+  );
 }
 
 export default Home;
