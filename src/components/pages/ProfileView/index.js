c7ed1c69d9f27ff290d912f995ecaa74395fe544
softdev1029 <softdev1029@outlook.com>
2022-04-05 22:16:19 +0300

    Make Profile View and Clicking the welcome message in the header should take the user to the Profile View

--- src/components/pages/ProfileView/index.js
+++ src/components/pages/ProfileView/index.js
@@ -1,7 +1,41 @@
 import React from "react";
 
+import data from "mock/data.json";
+
+import * as S from "./style";
+
 function ProfileView() {
-  return <div>Profile View</div>;
+  const { profile } = data;
+  return (
+    <S.Wrapper>
+      <h2>Profile</h2>
+      <S.Body>
+        <img src={profile.avatarImage} width={150} height={180} />
+        <S.Table>
+          <tr>
+            <S.Label>First name</S.Label>
+            <S.Value>{profile.firstName}</S.Value>
+          </tr>
+          <tr>
+            <S.Label>Last name</S.Label>
+            <S.Value>{profile.lastName}</S.Value>
+          </tr>
+          <tr>
+            <S.Label>Phone</S.Label>
+            <S.Value>{profile.phone}</S.Value>
+          </tr>
+          <tr>
+            <S.Label>Email</S.Label>
+            <S.Value>{profile.email}</S.Value>
+          </tr>
+          <tr>
+            <S.Label>Bio</S.Label>
+            <S.Value>{profile.bio}</S.Value>
+          </tr>
+        </S.Table>
+      </S.Body>
+    </S.Wrapper>
+  );
 }
 
 export default ProfileView;
---
+++ src/components/pages/ProfileView/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Body = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Table = styled.table`
+  width: 30%;
+  margin-left: 20px;
+`;
+
+export const Label = styled.td`
+  width: 30%;
+  font-weight: bold;
+`;
+
+export const Value = styled.td``;
